eff09ee07f5bfd33024e29dd3c44d817fef4125e
Igor Poliukhovych <igorpoliukhovychios@gmail.com>
2024-01-22 13:05:59 +0200

    created package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Preddio-iOS-SDK",
+    platforms: [.iOS(.v13), .macOS(.v12)],
+    products: [
+        .library(name: "AWSManager",
+                 targets: ["AWSManager"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", from: "2.33.8")
+    ],
+    targets: [
+        .target(
+            name: "AWSManager",
+            dependencies: [.product(name: "AWSIoT", package: "aws-sdk-ios-spm"),
+                           .product(name: "AWSCognitoIdentityProvider", package: "aws-sdk-ios-spm")],
+            path: "Sources"
+        )
+    ]
+)
---
+++ Sources/AWSManager/CoreEnvironment.swift
@@ -0,0 +1,18 @@
+import Foundation
+import AWSIoT
+
+enum Environment {
+    case test
+    case development
+    case production
+}
+
+struct AWSConfig {
+    var awsRegion: AWSRegionType
+    var ioTEndpoint: String
+    var identityPoolID: String
+    var awsIoTDataManagerKey: String
+    var poolID: String
+    var clientID: String
+    var userPoolKey: String
+}
